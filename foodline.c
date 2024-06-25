b053f45e46931c3f88fc2c06704d5e52367b1e62
Azabu-bound <tylerjbailey95@gmail.com>
2024-06-25 18:20:37 -0400

    Adding question for context

--- foodline.c
+++ foodline.c
@@ -1,3 +1,19 @@
+/*
+After many years of continuous warfare, the country of Collea was left in shambles. This has caused food shortages and famines all across the country. In an attempt to distribute the little food that was produced and prevent food hoarding, the Collean government limited the amount of food that any one person can buy. Soon, Collean citizens are forced to wait in long lines in order to obtain the food they needed. There are currently N such food lines in the city of Lachtin, the i th of which has a i people in it. M people are going to enter one of the lines in the next hour, where they each enter the shortest line they see. Since Phreia plans to enter the line, she wants to know the length of the line that each person decides to join.
+Constraints
+
+1 ≤ N ≤ 100
+1 ≤ a i ≤ 100 for i in 1 , 2 , … , N
+1 ≤ M ≤ 100
+Input Specification
+
+The first line contains two positive integers, N and M .
+The second line contains N positive integers, a 1 , a 2 , … , a N .
+Output Specification
+
+Print M lines, the i th of which being the length of the line that the i th person joined.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,7 +21,6 @@
 
 #define MAX_LINES 100
 
-// Create a helper function to find the shortest line
 int shortest_line_index(int lines[], int n) {
 	int shortest = 0;
 	for (int j = 1; j < n; ++j) {
