b7b559e2bd622e02b269aac7ac8809b537098a48
Azabu-bound <tylerjbailey95@gmail.com>
2024-08-24 12:02:53 -0400

    Setup prime exercise'

---
+++ c_practice/check_prime/main.c
@@ -0,0 +1,9 @@
+/*
+Task: Write a function int is_prime(int n) that checks if a number is prime.
+Test: is_prime(7) should return 1, is_prime(10) should return 0.
+*/
+
+int main()
+{
+  return 0;
+}
